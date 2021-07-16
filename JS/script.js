566b07f873619022d73fc1448451dfea39a39b16
Anthony64M <anthony.mutuku@students.moringaschool.com>
2021-07-16 13:57:08 +0300

    The Js file going well

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,12 @@
-var akan = function(y, m, d, g){
-    var 
+var akan = function(y, d, m, g){
+    var maleNames = ["Kwasi", "Kudwo" "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+    var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+    var d = new Date(y, d, --m);
+    if (g === "Male") {
+        return d && maleNames[d.getDay()];
+    }
+    else{
+        return d && femaleNames[d.getDay()];
+    }   
 }
+
